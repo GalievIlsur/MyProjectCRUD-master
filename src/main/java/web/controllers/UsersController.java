846c32cd41a64911fceeabbb526b13761e789e95
ILSUR26 <86534600+GalievIlsur@users.noreply.github.com>
2021-08-18 11:45:56 +0300

    Переделал контроллер.

--- src/main/java/web/controllers/UsersController.java
+++ src/main/java/web/controllers/UsersController.java
@@ -14,44 +14,44 @@ public class UsersController {
     @Autowired
     private UserService userServiceImp;
 
-    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
     public String getAllUsers(Model model) {
         model.addAttribute("users", userServiceImp.getAllUsers());
         return "users/getAllUsers";
     }
 
-    @GetMapping("/{id}")
-    public String getUser(@PathVariable("id") int id, Model model ) {
+    @RequestMapping("/getUser")
+    public String getUser(int id, Model model ) {
         model.addAttribute("user", userServiceImp.getUser(id));
         return "users/getUser";
     }
 
-    @GetMapping("/new")
+    @RequestMapping(value = "/new")
     public String newUser(Model model) {
         model.addAttribute("user", new User());
         return "users/new";
     }
 
-    @PostMapping
+    @RequestMapping
     public String create(@ModelAttribute("user") User user) {
         userServiceImp.save(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id) {
+    @RequestMapping("/edit")
+    public String edit(Model model, @RequestParam("id") int id) {
         model.addAttribute("user", userServiceImp.getUser(id));
         return "users/edit";
     }
 
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+    @RequestMapping(value = "/edit", method = RequestMethod.PATCH)
+    public String update(@ModelAttribute("user") User user, int id) {
         userServiceImp.update(id, user);
         return "redirect:/users";
     }
 
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
+    @RequestMapping(value = "/getUser", method = RequestMethod.DELETE)
+    public String delete(@RequestParam("id") int id) {
         userServiceImp.delete(id);
         return "redirect:/users";
     }
